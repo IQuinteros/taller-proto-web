026e0bdadd29bd1f8a7a251af6244eb5e00f7252
IQuinteros <i.quinteros@hotmail.com>
2021-06-13 17:05:37 -0400

    Conexión lista

--- api/conexiones/mi_perfil.php
+++ api/conexiones/mi_perfil.php
@@ -40,4 +40,98 @@ class MiPerfilConexion extends Conexion{
 
     }
 
+    public function editar($id, $nombre, $apellido, $edad, $descripcion, $hora_creacion, $es_genio, $fecha_especial, $precio)
+    {
+        $this->conectar();
+
+        $sql = "UPDATE mi_perfil SET nombre=:nombre, apellido=:apellido, edad=:edad, descripcion=:descripcion,
+        hora_creacion=:hora_creacion, es_genio=:es_genio, fecha_especial=:fecha_especial, precio=:precio WHERE id=:id";
+
+        $sentencia_pdo = $this->pdo->prepare($sql);
+    
+        $sentencia_pdo->bindParam(':id',                    $id,                PDO::PARAM_INT);
+        $sentencia_pdo->bindParam(':nombre',                $nombre,            PDO::PARAM_STR);
+        $sentencia_pdo->bindParam(':apellido',              $apellido,          PDO::PARAM_STR);
+        $sentencia_pdo->bindParam(':edad',                  $edad,              PDO::PARAM_INT);
+        $sentencia_pdo->bindParam(':descripcion',           $descripcion,       PDO::PARAM_STR);
+        $sentencia_pdo->bindParam(':hora_creacion',         $hora_creacion,     PDO::PARAM_STR);
+        $sentencia_pdo->bindParam(':es_genio',              $es_genio,          PDO::PARAM_BOOL);
+        $sentencia_pdo->bindParam(':fecha_especial',        $fecha_especial,    PDO::PARAM_STR);
+        $sentencia_pdo->bindParam(':precio',                $precio,            PDO::PARAM_STR);
+
+        try{
+            $funciona = $sentencia_pdo->execute();
+        } catch(PDOException $e){
+            $this->pdo = null;
+            return $e->getMessage();
+        }
+
+        $this->pdo = null;
+        return $funciona;
+
+    }
+
+    public function eliminar($id)
+    {
+        $this->conectar();
+
+        $sql = "DELETE FROM mi_perfil WHERE id=:id";
+
+        $sentencia_pdo = $this->pdo->prepare($sql);
+    
+        $sentencia_pdo->bindParam(':id',                    $id,                PDO::PARAM_INT);
+
+        try{
+            $funciona = $sentencia_pdo->execute();
+        } catch(PDOException $e){
+            $this->pdo = null;
+            return $e->getMessage();
+        }
+
+        $this->pdo = null;
+        return $funciona;
+    }
+
+    public function seleccionar()
+    {
+        $this->conectar();
+
+        $sql = "SELECT * FROM mi_perfil";
+
+        $sentencia_pdo = $this->pdo->prepare($sql);
+    
+        try{
+            $funciona = $sentencia_pdo->execute();
+        } catch(PDOException $e){
+            $this->pdo = null;
+            return $e->getMessage();
+        }
+
+        if(!$funciona){
+            $this->pdo = null;
+            return $funciona;
+        }
+        
+
+        // Devolver lista de perfiles
+        
+        $lista_perfiles = array();
+        $lista_perfiles = $sentencia_pdo->fetchAll(PDO::FETCH_OBJ);
+
+        $this->pdo = null;
+        return $lista_perfiles;
+    }
+
+    public function encontrar($id){
+        $lista = $this->seleccionar();
+
+        foreach($lista as $perfil){
+            if($perfil->id == $id){
+                return $perfil;
+            }
+        }
+
+        return null;
+    }
+
 }
--- api/testing.php
+++ api/testing.php
@@ -1,7 +1,18 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(~0);
+
 require_once __DIR__.("/conexiones/mi_perfil.php");
 
 $miPerfilConexion = new MiPerfilConexion();
 
 $miPerfilConexion->insertar("Hola", "apellido", 12, "Jajaja", date('Y/m/d H:i:s'), true, date('Y/m/d'), 20.5);
+
+$miPerfilConexion->editar(175, "CHAO", "Apellido 2", 5434534, "Jajaja", date('Y/m/d H:i:s'), true, date('Y/m/d'), 20.5);
+
+$miPerfilConexion->eliminar(185);
+
+echo json_encode($miPerfilConexion->seleccionar());
+
+echo json_encode($miPerfilConexion->encontrar(186));
